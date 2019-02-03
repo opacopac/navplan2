03acc24e4da755fd03b55b8f4b90560ca2b5091b
opacopac <armand@tschanz.com>
2019-02-03 17:49:20 +0100

    icao telephony designators importer

---
+++ src/data_import/data_import_icao_telephony_designators.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('max_execution_time', 600);
+
+include_once __DIR__ . "/../php/Navplan/Shared/DbService.php";
+
+use Navplan\Shared\DbService;
+
+$conn = DbService::openDb();
+
+// clear table
+$query = "TRUNCATE icao_telephony_designator";
+DbService::execCUDQuery($conn, $query);
+
+
+// importing airport data
+$filename = "./icao_telephony_designators/icao_telephony_designators.json";
+$abs_filename = realpath($filename);
+
+// skip directories
+if (!file_exists($abs_filename))
+	die("input file not found: " . $filename);
+
+$telDesignators = json_decode(file_get_contents($abs_filename), true);
+
+foreach ($telDesignators as $designator => $telephony) {
+	$query = "INSERT INTO icao_telephony_designator (designator, telephony) VALUES (";
+	$query .= " '" . mysqli_real_escape_string($conn->getMySqli(), $designator) . "',";
+	$query .= " '" . mysqli_real_escape_string($conn->getMySqli(), $telephony) . "'";
+	$query .= ")";
+
+	$result = DbService::execCUDQuery($conn, $query);
+}
+
+print "done.";
