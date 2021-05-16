2055cfa109d2c92a5feac7cd8165c1d9a319413e
opacopac <armand@tschanz.com>
2021-05-16 14:50:42 +0200

    moving ngrx to components

--- src/app/aerodrome/ngrx/airport-chart.reducer.ts
+++ src/app/aerodrome/ngrx/airport-chart.reducer.ts
@@ -4,7 +4,7 @@ import {AirportChartState} from '../domain-model/airport-chart-state';
 
 
 const initialState: AirportChartState = {
-    airportCharts: [],
+    airportCharts: []
 };
 
 
@@ -12,13 +12,10 @@ export const airportChartReducer = createReducer(
     initialState,
     on(AirportChartActions.showAirportChart, (state, action) => ({
         ...state,
-        airportChartState: { airportCharts: [...state.airportCharts, action.chart ] },
-        showOverlay: { dataItem: undefined, clickPos: undefined }
+        airportCharts: [...state.airportCharts, action.chart ]
     })),
     on(AirportChartActions.closeAirportChart, (state, action) => ({
         ...state,
-        airportChartState: {
-            airportCharts: [ ...state.airportCharts.filter(chart => chart.id !== action.chartId) ]
-        },
+        airportCharts: [ ...state.airportCharts.filter(chart => chart.id !== action.chartId) ]
     })),
 );
