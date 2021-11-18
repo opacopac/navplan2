f61d3819e68f9aee9426a4bb54c0569e998fb0a5
opacopac <armand@tschanz.com>
2021-11-18 23:01:14 +0100

    flightmap

---
+++ src/app/flight-map/domain-service/flightmap.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {Extent2d} from '../../common/geo-math/domain-model/geometry/extent2d';
+import {IFlightmapService} from './i-flightmap.service';
+import {AirportState} from '../domain-model/airport-state';
+
+
+@Injectable()
+export class FlightmapService implements IFlightmapService {
+    constructor() {
+    }
+
+
+    public readByExtent(extent: Extent2d, zoom: number): Observable<AirportState> {
+        return of(undefined);
+    }
+}
---
+++ src/app/flight-map/domain-service/i-flightmap.service.ts
@@ -0,0 +1,8 @@
+import {Observable} from 'rxjs';
+import {Extent2d} from '../../common/geo-math/domain-model/geometry/extent2d';
+import {AirportState} from '../domain-model/airport-state';
+
+
+export abstract class IFlightmapService {
+    public abstract readByExtent(extent: Extent2d, zoom: number): Observable<AirportState>;
+}
