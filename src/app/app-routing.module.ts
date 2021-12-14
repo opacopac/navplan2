fefd783f703ad2b677684f4e0de35c5417a8aeba
opacopac <armand@tschanz.com>
2021-12-14 23:39:09 +0100

    flight prep menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,29 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SettingsPageComponent} from './settings-page-view/ng-components/settings-page/settings-page.component';
 import {AboutPageComponent} from './about-view/ng-components/about-page/about-page.component';
-import {UserProfilePageComponent} from './user-view/ng-components/user-profile/user-profile-page/user-profile-page.component';
+import {
+    UserProfilePageComponent
+} from './user-view/ng-components/user-profile/user-profile-page/user-profile-page.component';
 import {FlightMapPageComponent} from './flight-map-view/ng-components/flight-map-page/flight-map-page.component';
 import {TracksPageComponent} from './track-view/ng-components/tracks-page/tracks-page.component';
-import {LoginRegisterPageComponent} from './user-view/ng-components/user-profile/login-register-page/login-register-page.component';
-import {RegisterStep2PageComponent} from './user-view/ng-components/user-profile/register-step2-page/register-step2-page.component';
-import {ForgotPwStep1PageComponent} from './user-view/ng-components/user-profile/forgot-pw-step1-page/forgot-pw-step1-page.component';
-import {ForgotPwStep2PageComponent} from './user-view/ng-components/user-profile/forgot-pw-step2-page/forgot-pw-step2-page.component';
+import {
+    LoginRegisterPageComponent
+} from './user-view/ng-components/user-profile/login-register-page/login-register-page.component';
+import {
+    RegisterStep2PageComponent
+} from './user-view/ng-components/user-profile/register-step2-page/register-step2-page.component';
+import {
+    ForgotPwStep1PageComponent
+} from './user-view/ng-components/user-profile/forgot-pw-step1-page/forgot-pw-step1-page.component';
+import {
+    ForgotPwStep2PageComponent
+} from './user-view/ng-components/user-profile/forgot-pw-step2-page/forgot-pw-step2-page.component';
 import {FlightPrepPageComponent} from './flight-prep-view/ng-components/flight-prep-page/flight-prep-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'map', pathMatch: 'full' },
     { path: 'map', component: FlightMapPageComponent },
-    { path: 'prep', component: FlightPrepPageComponent },
+    { path: 'prep/:tab', component: FlightPrepPageComponent },
     { path: 'tracks', component: TracksPageComponent },
     { path: 'login', component: LoginRegisterPageComponent },
     { path: 'register/:token', component: RegisterStep2PageComponent },
--- src/app/flight-prep-view/ng-components/flight-prep-page/flight-prep-page.component.ts
+++ src/app/flight-prep-view/ng-components/flight-prep-page/flight-prep-page.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {map} from 'rxjs/operators';
+import {Observable, Subscription} from 'rxjs';
+import {MatTabGroup} from '@angular/material/tabs';
 
 
 @Component({
@@ -6,11 +10,50 @@ import {Component, OnInit} from '@angular/core';
     templateUrl: './flight-prep-page.component.html',
     styleUrls: ['./flight-prep-page.component.css'],
 })
-export class FlightPrepPageComponent implements OnInit {
-    constructor() {
+export class FlightPrepPageComponent implements OnInit, AfterViewInit, OnDestroy {
+    public readonly tab$: Observable<string>;
+    public tabSubscription: Subscription;
+    @ViewChild('tabGroup') public tabGroup: MatTabGroup;
+
+
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+        this.tab$ = route.params.pipe(
+            map(params => params['tab'])
+        );
     }
 
 
     ngOnInit() {
+    }
+
+
+    ngAfterViewInit() {
+        this.tabSubscription = this.tab$.subscribe(tab => {
+            if (this.tabGroup) {
+                this.tabGroup.selectedIndex = this.getIndex(tab);
+            }
+        });
+    }
+
+
+    ngOnDestroy() {
+        this.tabSubscription.unsubscribe();
+    }
+
+
+    private getIndex(tab: string): number {
+        switch (tab) {
+            case 'fuel': return 1;
+            case 'wnb': return 2;
+            case 'perf': return 3;
+            case 'meteo': return 4;
+            case 'notam': return 5;
+            case 'route':
+            default:
+                return 0;
+        }
     }
 }
