9e5118932c87da22b42f74f3d1078c63737522d7
opacopac <armand@tschanz.com>
2022-02-21 00:06:12 +0100

    static styles

--- src/app/webcam-view/ol-components/ol-webcam.ts
+++ src/app/webcam-view/ol-components/ol-webcam.ts
@@ -1,10 +1,8 @@
-import {Icon, Style} from 'ol/style';
-import {environment} from '../../../environments/environment';
 import {Webcam} from '../../webcam/domain-model/webcam';
-import IconAnchorUnits from 'ol/style/IconAnchorUnits';
 import {OlFeature} from '../../base-map-view/ol-model/ol-feature';
 import {OlVectorLayer} from '../../base-map-view/ol-model/ol-vector-layer';
 import {OlGeometry} from '../../base-map-view/ol-model/ol-geometry';
+import {OlWebcamStyle} from './ol-webcam-style';
 
 
 export class OlWebcam {
@@ -13,24 +11,8 @@ export class OlWebcam {
         layer: OlVectorLayer
     ) {
         const olFeature = new OlFeature(webcam, true);
-        olFeature.setStyle(this.createPointStyle());
+        olFeature.setStyle(OlWebcamStyle.STYLE);
         olFeature.setGeometry(OlGeometry.fromPoint(webcam.position));
         layer.addFeature(olFeature);
-    }
-
-
-    private static createPointStyle(): Style {
-        const src = environment.iconBaseUrl;
-
-        return new Style({
-            image: new Icon(({
-                anchor: [0.5, 0.5],
-                anchorXUnits: IconAnchorUnits.FRACTION,
-                anchorYUnits: IconAnchorUnits.FRACTION,
-                scale: 1,
-                opacity: 0.9,
-                src: src + 'webcam.svg'
-            }))
-        });
     }
 }
