0be716839886eae4dc9896fefdd1f3a2bca04634
opacopac <armand@tschanz.com>
2022-04-24 18:17:16 +0200

    dwd forecast overlay

--- src/app/meteo-dwd-view/ol-components/ol-dwd-forecast-tilelayer.ts
+++ src/app/meteo-dwd-view/ol-components/ol-dwd-forecast-tilelayer.ts
@@ -6,6 +6,7 @@ import {OlTileLayer} from '../../base-map-view/ol-model/ol-tile-layer';
 export class OlDwdForecastTilelayer {
     public static create(): OlTileLayer {
         const layer = new TileLayer({
+            opacity: 0.8,
             source: new XYZ({
                 url: 'http://localhost/navplan2/maptiles/meteo_test/{z}/{x}/{y}.png',
                 maxZoom: 7,
