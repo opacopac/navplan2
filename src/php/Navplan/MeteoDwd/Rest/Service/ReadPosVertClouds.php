acd9c9ed46c9f4ab6593c2c647f301eeef762663
Armand Tschanz <armand@tschanz.com>
2023-06-23 18:53:47 +0200

    positional meteogram

--- src/php/Navplan/MeteoDwd/Rest/Service/ReadPosVertClouds.php
+++ src/php/Navplan/MeteoDwd/Rest/Service/ReadPosVertClouds.php
@@ -4,7 +4,7 @@ namespace Navplan\MeteoDwd\RestService;
 
 use Navplan\Common\Rest\Converter\RestPosition2dConverter;
 use Navplan\MeteoDwd\Rest\Model\RestForecastRunConverter;
-use Navplan\MeteoDwd\Rest\Model\RestVerticalCloudColumnConverter;
+use Navplan\MeteoDwd\Rest\Model\RestVerticalCloudColumnStepConverter;
 use Navplan\ProdNavplanDiContainer;
 
 require_once __DIR__ . "/../../../RestServiceBootstrap.php";
@@ -23,4 +23,4 @@ $forecastRun = RestForecastRunConverter::fromRest($args[ARG_FORECAST_RUN]);
 $pos = RestPosition2dConverter::fromRest([$args[ARG_POS_LON], $args[ARG_POS_LAT]]);
 
 $verticalCloudColumnSteps = $verticalCloudService->readPositionalVerticalClouds($forecastRun, $pos);
-$httpService->sendArrayResponse(RestVerticalCloudColumnConverter::toRestList($verticalCloudColumnSteps));
+$httpService->sendArrayResponse(RestVerticalCloudColumnStepConverter::toRestList($verticalCloudColumnSteps));
