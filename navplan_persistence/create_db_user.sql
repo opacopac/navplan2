c65a84502f1fe064d3c6f07cc294d3accd87a5a3
opacopac <armand@tschanz.com>
2023-10-02 23:33:14 +0200

    docker secrets for db credentials

--- navplan_persistence/create_db_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
---CREATE USER 'username'@'%' IDENTIFIED BY 'password';
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON tschanz_navplan.* TO 'username'@'localhost';
---GRANT ALL PRIVILEGES ON * . * TO 'username'@'%';
-
-FLUSH PRIVILEGES;
