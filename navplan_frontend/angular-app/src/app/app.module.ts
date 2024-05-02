f15286030facf15f89f0586fd782ef9c7880eb41
opacopac <armand@tschanz.com>
2024-05-02 22:47:28 +0200

    updating angular material to v17

--- navplan_frontend/angular-app/src/app/app.module.ts
+++ navplan_frontend/angular-app/src/app/app.module.ts
@@ -36,6 +36,7 @@ import {MessageViewModule} from './message/view/message-view.module';
         StoreDevtoolsModule.instrument({
             maxAge: 25, // Retains last 25 states
             logOnly: environment.production, // Restrict extension to log-only mode
+            connectInZone: true
         }),
         AboutViewModule,
         ExporterViewModule,
