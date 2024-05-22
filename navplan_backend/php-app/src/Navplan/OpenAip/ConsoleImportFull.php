1b05251b553ab7ad700478dc776b04de824313c8
opacopac <armand@tschanz.com>
2024-05-22 23:05:45 +0200

    openapi import scripts

--- navplan_backend/php-app/src/Navplan/OpenAip/ConsoleImportFull.php
+++ navplan_backend/php-app/src/Navplan/OpenAip/ConsoleImportFull.php
@@ -2,7 +2,6 @@
 
 namespace Navplan\OpenAip;
 
-use Navplan\OpenAip\ApiAdapter\Service\OpenAipImportFilter;
 use Navplan\ProdNavplanDiContainer;
 
 
@@ -11,9 +10,7 @@ require_once __DIR__ . "/../ConsoleBootstrap.php";
 
 $diContainer = new ProdNavplanDiContainer();
 
-$importFilter = new OpenAipImportFilter("CH");
 $importer = $diContainer->getOpenAipDiContainer()->getOpenAipImporter();
-$importer->setImportFilter($importFilter);
 $importer->importNavaids();
 $importer->importAirports();
 $importer->importAirspaces();
