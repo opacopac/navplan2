d742370fab7ba4721f521b7122598abb599a899e
opacopac <armand@tschanz.com>
2024-05-22 23:42:07 +0200

    openapi import scripts fix

--- navplan_backend/php-app/src/Navplan/OpenAip/DataCorrectionScripts/correct_airspaces.sql
+++ navplan_backend/php-app/src/Navplan/OpenAip/DataCorrectionScripts/correct_airspaces.sql
@@ -1,4 +1,4 @@
-update openaip_airspace2 set type = 'FIZ', category = 'FIZ' where name = 'FIZ SAMEDAN';
+update openaip_airspace2 set type = 'FIZ', category = 'FIZ' where name = 'FIZ SAMEDAN 135.325';
 
 delete from openaip_airspace2
 where name in (
