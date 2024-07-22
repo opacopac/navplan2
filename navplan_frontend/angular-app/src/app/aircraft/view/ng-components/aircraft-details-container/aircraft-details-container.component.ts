d80e54f53a6f687c59e9cad3d377f42fdfb62032
Armand Tschanz <armand@tschanz.com>
2024-07-22 20:31:09 +0200

    aircraaft list

---
+++ navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-details-container/aircraft-details-container.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'app-aircraft-details-container',
+    templateUrl: './aircraft-details-container.component.html',
+    styleUrls: ['./aircraft-lisdetailst-container.component.scss'],
+})
+export class AircraftDetailsContainerComponent implements OnInit {
+    constructor() {
+    }
+
+
+    ngOnInit() {
+    }
+}
---
+++ navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-list-container/aircraft-list-container.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'app-aircraft-list-container',
+    templateUrl: './aircraft-list-container.component.html',
+    styleUrls: ['./aircraft-list-container.component.scss'],
+})
+export class AircraftListContainerComponent implements OnInit {
+    constructor() {
+    }
+
+
+    ngOnInit() {
+    }
+}
