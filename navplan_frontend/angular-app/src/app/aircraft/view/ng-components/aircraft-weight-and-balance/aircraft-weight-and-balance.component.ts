502e73e7457083af4c9cb2569824c38ca746610c
opacopac <armand@tschanz.com>
2024-07-27 18:35:17 +0200

    aircraft performance / w&b

--- navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-weight-and-balance/aircraft-weight-and-balance.component.ts
+++ navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-weight-and-balance/aircraft-weight-and-balance.component.ts
@@ -2,6 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Aircraft} from '../../../domain/model/aircraft';
 import {SpeedUnit} from '../../../../geo-physics/domain/model/quantities/speed-unit';
 import {WeightUnit} from '../../../../geo-physics/domain/model/quantities/weight-unit';
+import {Speed} from '../../../../geo-physics/domain/model/quantities/speed';
+import {Weight} from '../../../../geo-physics/domain/model/quantities/weight';
+import {FormControl, Validators} from '@angular/forms';
 
 
 export interface ListEntry {
@@ -21,11 +24,31 @@ export class AircraftWeightAndBalanceComponent implements OnInit {
     @Input() speedUnit: SpeedUnit;
     @Input() weightUnit: WeightUnit;
 
+    protected mtowInput: FormControl;
+    protected bewInput: FormControl;
+    protected readonly Speed = Speed;
+    protected readonly Weight = Weight;
 
     constructor() {
     }
 
 
     ngOnInit() {
+        const mtowValue = this.currentAircraft.mtow
+            ? this.currentAircraft.mtow.getValue(this.weightUnit).toString()
+            : '';
+        this.mtowInput = new FormControl(mtowValue, [
+            Validators.required,
+            Validators.min(1),
+            Validators.max(99999)
+        ]);
+        const bewValue = this.currentAircraft.bew
+            ? this.currentAircraft.bew.getValue(this.weightUnit).toString()
+            : '';
+        this.bewInput = new FormControl(bewValue, [
+            Validators.required,
+            Validators.min(1),
+            Validators.max(99999)
+        ]);
     }
 }
