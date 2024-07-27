393971193341c5b2e97044575be8a3618d35b9e0
opacopac <armand@tschanz.com>
2024-07-27 16:15:01 +0200

    aircraft performance / w&b

---
+++ navplan_backend/php-app/src/Navplan/Aircraft/Persistence/Query/DbWnbEnvelopeConverter.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Navplan\Aircraft\Persistence\Query;
+
+use Navplan\Aircraft\Domain\Model\WnbEnvelope;
+use Navplan\Aircraft\Domain\Model\WnbEnvelopeCoordinate;
+use Navplan\Aircraft\Persistence\Model\DbTableAircraftWnbEnvelopes;
+use Navplan\Common\Domain\Model\Length;
+use Navplan\Common\Domain\Model\LengthUnit;
+use Navplan\Common\Domain\Model\Weight;
+use Navplan\Common\Domain\Model\WeightUnit;
+
+
+class DbWnbEnvelopeConverter
+{
+    public static function fromDbRow(array $row): WnbEnvelope
+    {
+        return new WnbEnvelope(
+            $row[DbTableAircraftWnbEnvelopes::COL_NAME],
+            self::parseCoordinates($row[DbTableAircraftWnbEnvelopes::COL_COORDINATES_KG_M])
+        );
+    }
+
+
+    /**
+     * @param string $jsonString
+     * @return WnbEnvelopeCoordinate[]
+     */
+    private static function parseCoordinates(string $jsonString): array
+    {
+        return array_map(function ($jsonCoord) {
+            return new WnbEnvelopeCoordinate(
+                new Weight(floatval(trim($jsonCoord[0])), WeightUnit::KG),
+                new Length(floatval(trim($jsonCoord[1])), LengthUnit::M)
+            );
+        }, json_decode($jsonString, true));
+    }
+}
