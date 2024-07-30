a6abf4ffed52e418b570c8ea7e10a39e2cf46512
opacopac <armand@tschanz.com>
2024-07-30 20:50:45 +0200

    aircraft details

--- navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-details/aircraft-details.component.ts
+++ navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-details/aircraft-details.component.ts
@@ -1,17 +1,10 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Aircraft} from '../../../domain/model/aircraft';
-import {FormControl, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Consumption} from '../../../../geo-physics/domain/model/quantities/consumption';
 import {Speed} from '../../../../geo-physics/domain/model/quantities/speed';
 import {SpeedUnit} from '../../../../geo-physics/domain/model/quantities/speed-unit';
 import {ConsumptionUnit} from '../../../../geo-physics/domain/model/quantities/consumption-unit';
-
-
-export interface ListEntry {
-    id: number;
-    registration: string;
-    type: string;
-}
 
 
 @Component({
@@ -27,39 +20,40 @@ export class AircraftDetailsComponent implements OnInit {
 
     protected readonly Speed = Speed;
     protected readonly Consumption = Consumption;
-    protected registrationInput: FormControl;
-    protected typeInput: FormControl;
-    protected speedInput: FormControl;
-    protected consumptionInput: FormControl;
+    protected aircraftDetailsForm: FormGroup;
 
 
-    constructor() {
+    constructor(private formBuilder: FormBuilder) {
     }
 
 
     ngOnInit() {
-        this.registrationInput = new FormControl(this.currentAircraft.registration, [
-            Validators.required
-        ]);
-        this.typeInput = new FormControl(this.currentAircraft.icaoType, [
-            Validators.required
-        ]);
-        const speedValue = this.currentAircraft.cruiseSpeed
-            ? this.currentAircraft.cruiseSpeed.getValue(this.speedUnit).toString()
-            : '';
-        this.speedInput = new FormControl(speedValue, [
-            Validators.required,
-            Validators.min(1),
-            Validators.max(999)
-        ]);
-        const consumptionValue = this.currentAircraft.cruiseFuel
-            ? this.currentAircraft.cruiseFuel.getValue(this.consumptionUnit).toString()
-            : '';
-        this.consumptionInput = new FormControl(consumptionValue, [
-            Validators.required,
-            Validators.min(1),
-            Validators.max(999)
-        ]);
+        this.aircraftDetailsForm = this.formBuilder.group({
+            'registration': [this.currentAircraft.registration, [
+                Validators.required
+            ]],
+            'type': [this.currentAircraft.icaoType, [
+                Validators.required
+            ]],
+            'speed': [this.currentAircraft.cruiseSpeed
+                ? this.currentAircraft.cruiseSpeed.getValue(this.speedUnit).toString()
+                : '',
+                [
+                    Validators.required,
+                    Validators.min(1),
+                    Validators.max(999)
+                ]
+            ],
+            'consumption': [this.currentAircraft.cruiseFuel
+                ? this.currentAircraft.cruiseFuel.getValue(this.consumptionUnit).toString()
+                : '',
+                [
+                    Validators.required,
+                    Validators.min(1),
+                    Validators.max(999)
+                ]
+            ]
+        });
     }
 
 
@@ -70,5 +64,12 @@ export class AircraftDetailsComponent implements OnInit {
 
     protected isHelicopter(): boolean {
         return this.currentAircraft.vehicleType === 'HELICOPTER';
+    }
+
+
+    protected onSaveAircraftDetailsClicked() {
+        if (this.aircraftDetailsForm.valid) {
+            this.onSaveAircraftClick.emit(null); // TODO
+        }
     }
 }
