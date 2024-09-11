ad6d7d74713c579b00d228ac4248a6914161dbf6
opacopac <armand@tschanz.com>
2024-09-12 00:47:08 +0200

    aircraft type designator

--- navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-details/aircraft-type-designator-autocomplete/aircraft-type-designator-autocomplete.component.ts
+++ navplan_frontend/angular-app/src/app/aircraft/view/ng-components/aircraft-details/aircraft-type-designator-autocomplete/aircraft-type-designator-autocomplete.component.ts
@@ -2,8 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {AircraftTypeDesignatorActions} from '../../../../state/ngrx/aircraft-type-designator.actions';
 import {AircraftTypeDesignator} from '../../../../domain/model/aircraft-type-designator';
-import {AircraftType} from '../../../../domain/model/aircraft-type';
-import {EngineType} from '../../../../domain/model/engine-type';
 import {AutoCompleteResultItem} from '../../../../../common/view/model/auto-complete-result-item';
 import {getAcTypeDesignatorSearchResults} from '../../../../state/ngrx/aircraft.selectors';
 import {map} from 'rxjs/operators';
@@ -44,16 +42,12 @@ export class AircraftTypeDesignatorAutocompleteComponent implements OnInit {
     }
 
 
-    private createFakeAircraftTypeDesignatorList(): AircraftTypeDesignator[] {
-        return [
-            new AircraftTypeDesignator(1, 'BR23', 'Bristell B23', 'Bristell Aero', AircraftType.LANDPLANE, EngineType.PISTON, 1, 'L'),
-            new AircraftTypeDesignator(2, 'BR24', 'Bristell B24', 'Bristell Aero', AircraftType.LANDPLANE, EngineType.PISTON, 1, 'L'),
-            new AircraftTypeDesignator(3, 'BR25', 'Bristell B25', 'Bristell Aero', AircraftType.LANDPLANE, EngineType.PISTON, 1, 'L')
-        ];
-    }
-
-
     private toSearchResultItems(acTypeDesignators: AircraftTypeDesignator[]): AutoCompleteResultItem<AircraftTypeDesignator>[] {
-        return acTypeDesignators ? acTypeDesignators.map(typeDesignator => new AutoCompleteResultItem(typeDesignator, typeDesignator.designator)) : [];
+        return acTypeDesignators
+            ? acTypeDesignators.map(typeDesignator => new AutoCompleteResultItem(
+                typeDesignator,
+                typeDesignator.designator + ' - ' + typeDesignator.model + ' - ' + typeDesignator.manufacturer + ''
+            ))
+            : [];
     }
 }
