dee58310d510849af507e3a8ebe6451ef36f04d4
Armand Tschanz <armand@tschanz.com>
2024-09-21 18:22:24 +0200

    plan performance service

--- navplan_frontend/angular-app/src/app/flightroute/domain/service/plan-performance.service.ts
+++ navplan_frontend/angular-app/src/app/flightroute/domain/service/plan-performance.service.ts
@@ -4,7 +4,7 @@ import {DistancePerformanceTable} from '../../../aircraft/domain/model/distance-
 import {Speed} from '../../../geo-physics/domain/model/quantities/speed';
 import {Length} from '../../../geo-physics/domain/model/quantities/length';
 import {AtmosphereService} from '../../../geo-physics/domain/service/meteo/atmosphere.service';
-import {PerformanceTableAltitudeReference} from '../../../aircraft/domain/model/performance-table-altitude-reference';
+import { PerformanceTableTemperatureReference } from '../../../aircraft/domain/model/performance-table-temperature-reference';
 
 export class PlanPerformanceService {
     public static calcTakeOffGroundRoll(
@@ -17,11 +17,51 @@ export class PlanPerformanceService {
         headwind: Speed,
         tailwind: Speed,
         performanceTable: DistancePerformanceTable
-    ): number {
-        const lookupAlt = performanceTable.altitudeReference === PerformanceTableAltitudeReference.FIELD_ALTITUDE
-            ? elevation
-            : AtmosphereService.calcPressureAltitude(elevation, qnh);
+    ): Length {
         const pa = AtmosphereService.calcPressureAltitude(elevation, qnh);
-        return 0;
+        const isaTemp = AtmosphereService.calcIsaTemperature(pa, oat);
+        const temp = performanceTable.temperatureReference === PerformanceTableTemperatureReference.ISA_TEMPERATURE
+        ? isaTemp
+        : oat;
+
+        const altIdx = this.findFractionalIndex(pa, performanceTable.altitudeSteps, step => step.ft);
+        const tempIdx = this.findFractionalIndex(temp, performanceTable.temperatureSteps, step => step.c);
+
+        return Length.createZero();
+    }
+
+
+    private static findFractionalIndex<T>(
+        value: T,
+        valueList: T[],
+        getValueFn: (length: T) => number
+    ): number {
+        const numValue = getValueFn(value);
+
+        if (valueList.length === 0) {
+            throw new Error("Value list cannot be empty.");
+        }
+    
+        if (numValue <= getValueFn(valueList[0])) {
+            return 0;
+        }
+    
+        if (numValue >= getValueFn(valueList[valueList.length - 1])) {
+            return valueList.length - 1;
+        }
+    
+        // Find the two integers between which the value lies
+        for (let i = 0; i < valueList.length - 1; i++) {
+            const lowerNumValue = getValueFn(valueList[i]);
+            const upperNumValue = getValueFn(valueList[i + 1]);
+    
+            if (numValue >= lowerNumValue && numValue <= upperNumValue) {
+                // Perform linear interpolation
+                const fraction = (numValue - lowerNumValue) / (upperNumValue - lowerNumValue);
+                return i + fraction;
+            }
+        }
+    
+        throw new Error("Value is out of bounds or list is not sorted.");
     }
 }
