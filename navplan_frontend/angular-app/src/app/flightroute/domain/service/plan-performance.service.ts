1ded32834852a964059f20531169524b2ff2ef8c
Armand Tschanz <armand@tschanz.com>
2024-09-28 20:44:48 +0200

    performance service

--- navplan_frontend/angular-app/src/app/flightroute/domain/service/plan-performance.service.ts
+++ navplan_frontend/angular-app/src/app/flightroute/domain/service/plan-performance.service.ts
@@ -10,28 +10,43 @@ import { RwyCorrectionFactors } from '../model/rwy-correction-factors';
 
 export class PlanPerformanceService {
     public static calcTakeOffGroundRoll(
-        elevation: Length,
+        fieldElevation: Length,
         qnh: Pressure,
         oat: Temperature,
         rwyCorrectionFactors: RwyCorrectionFactors,
         performanceTable: DistancePerformanceTable
     ): Length {
-        const pa = AtmosphereService.calcPressureAltitude(elevation, qnh);
+        const pa = AtmosphereService.calcPressureAltitude(fieldElevation, qnh);
         const isaTemp = AtmosphereService.calcIsaTemperature(pa, oat);
         const temp = performanceTable.temperatureReference === PerformanceTableTemperatureReference.ISA_TEMPERATURE
             ? isaTemp
             : oat;
 
         const altIdx = ArrayHelper.findFractionalIndex(pa, performanceTable.altitudeSteps, alt => alt.ft);
+        const altLower = performanceTable.altitudeSteps[Math.floor(altIdx)];
+        const altUpper = performanceTable.altitudeSteps[Math.ceil(altIdx)];
+        const altDiffFt = altUpper.ft - altLower.ft;
+
         const tempIdx = ArrayHelper.findFractionalIndex(temp, performanceTable.temperatureSteps, temp => temp.c);
+        const tempLower = performanceTable.temperatureSteps[Math.floor(tempIdx)];
+        const tempUpper = performanceTable.temperatureSteps[Math.ceil(tempIdx)];
+        const tempDiffC = tempUpper.c - tempLower.c;
 
         const rollLowerAltLowerTemp = performanceTable.distanceValues[Math.floor(altIdx)][Math.floor(tempIdx)];
         const rollLowerAltUpperTemp = performanceTable.distanceValues[Math.floor(altIdx)][Math.ceil(tempIdx)];
-        //const rollLower = rollLowerAltLowerTemp + (rollLowerAltUpperTemp - rollLowerAltLowerTemp) * (temp.c - performanceTable.temperatureSteps[Math.floor(tempIdx)].c);
+        const lowerRollDiffM = rollLowerAltUpperTemp.m - rollLowerAltLowerTemp.m;
+        const lowerRollM = rollLowerAltLowerTemp.m + lowerRollDiffM * (isaTemp.c - tempLower.c) / tempDiffC;
 
         const rollUpperAltLowerTemp = performanceTable.distanceValues[Math.ceil(altIdx)][Math.floor(tempIdx)];
         const rollUpperAltUpperTemp = performanceTable.distanceValues[Math.ceil(altIdx)][Math.ceil(tempIdx)];
+        const upperRollDiffM = rollUpperAltUpperTemp.m - rollUpperAltLowerTemp.m;
+        const upperRollM = rollUpperAltLowerTemp.m + upperRollDiffM * (isaTemp.c - tempLower.c) / tempDiffC;
 
-        return null;
+        const rollDiffM = upperRollM - lowerRollM;
+        const rollM = lowerRollM + rollDiffM * (pa.ft - altLower.ft) / altDiffFt;
+
+        // TODO correction factors
+
+        return Length.ofM(rollM);
     }
 }
