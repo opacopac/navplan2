9cddbc53b49cbf321cebd9aa8568c4d354bc0f84
opacopac <armand@tschanz.com>
2025-01-02 15:12:38 +0100

    performance calculation view

--- navplan_frontend/angular-app/src/app/plan-performance/state/ngrx/plan-perf.reducer.ts
+++ navplan_frontend/angular-app/src/app/plan-performance/state/ngrx/plan-perf.reducer.ts
@@ -7,48 +7,39 @@ import {Pressure} from '../../../geo-physics/domain/model/quantities/pressure';
 import {Temperature} from '../../../geo-physics/domain/model/quantities/temperature';
 import {Speed} from '../../../geo-physics/domain/model/quantities/speed';
 import {MockRwyLszb32} from '../../../aerodrome/domain/mock/mock-rwy-lszb-32';
+import {PlanPerfWeatherFactorsState} from '../state-model/plan-perf-weather-factors-state';
 
 
 // TODO: initial state
+const initialWeatherFactors: PlanPerfWeatherFactorsState = {qnh: Pressure.ofHpa(1013), oat: Temperature.ofC(15)};
+const initialRunwayFactors = {
+    isGrassRwy: false,
+    isWetRwy: false,
+    rwySlopePercent: 0,
+    rwyWind: Speed.ofZero(),
+    reservePercent: 0
+};
 const initialState: PlanPerfState = {
     departureAirportState: {
         airport: MockAirportLszb.create(),
         runway: MockRwyLszb14.create(),
         aircraftPerfProfileIdx: 0,
-        weatherFactors: {qnh: Pressure.ofHpa(1013), oat: Temperature.ofC(15)},
-        runwayFactors: {
-            isGrassRwy: false,
-            isWetRwy: false,
-            rwySlopePercent: 0,
-            rwyWind: Speed.ofZero(),
-            reservePercent: 0
-        },
+        weatherFactors: initialWeatherFactors,
+        runwayFactors: initialRunwayFactors,
     },
     destinationAirportState: {
         airport: MockAirportLszb.create(),
         runway: MockRwyLszb32.create(),
         aircraftPerfProfileIdx: 0,
-        weatherFactors: {qnh: Pressure.ofHpa(1013), oat: Temperature.ofC(15)},
-        runwayFactors: {
-            isGrassRwy: false,
-            isWetRwy: false,
-            rwySlopePercent: 0,
-            rwyWind: Speed.ofZero(),
-            reservePercent: 0
-        },
+        weatherFactors: initialWeatherFactors,
+        runwayFactors: initialRunwayFactors,
     },
     alternateAirportState: {
         airport: null,
         runway: null,
         aircraftPerfProfileIdx: 0,
-        weatherFactors: {qnh: Pressure.ofHpa(1013), oat: Temperature.ofC(15)},
-        runwayFactors: {
-            isGrassRwy: false,
-            isWetRwy: false,
-            rwySlopePercent: 0,
-            rwyWind: Speed.ofZero(),
-            reservePercent: 0
-        },
+        weatherFactors: initialWeatherFactors,
+        runwayFactors: initialRunwayFactors,
     },
 };
 
