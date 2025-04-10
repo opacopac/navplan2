755ab637f2453e99e1e57e8871c92d69d631bc46
opacopac <armand@tschanz.com>
2025-04-10 19:03:14 +0200

    modularizing flightmap

--- navplan_frontend/angular-app/src/app/flight-map/state/flight-map-state.module.ts
+++ navplan_frontend/angular-app/src/app/flight-map/state/flight-map-state.module.ts
@@ -4,12 +4,14 @@ import {EffectsModule} from '@ngrx/effects';
 import {flightMapReducer} from './ngrx/flight-map.reducer';
 import {FlightMapEffects} from './ngrx/flight-map.effects';
 import {FlightMapStateService} from './ngrx/flight-map-state.service';
+import {FlightMapDomainModule} from '../domain/flight-map-domain.module';
 
 
 @NgModule({
     imports: [
         StoreModule.forFeature('flightMapState', flightMapReducer),
         EffectsModule.forFeature([FlightMapEffects]),
+        FlightMapDomainModule
     ],
     declarations: [],
     exports: [],
