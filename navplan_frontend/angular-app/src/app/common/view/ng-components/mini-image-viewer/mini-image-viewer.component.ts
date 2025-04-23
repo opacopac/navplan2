79570fdba3f2a8e657d814c244e7db6950c17ed8
ue55617 <armand.tschanz@sbb.ch>
2025-04-23 17:49:11 +0200

    mini image viewer

---
+++ navplan_frontend/angular-app/src/app/common/view/ng-components/mini-image-viewer/mini-image-viewer.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {CommonModule} from '@angular/common';
+
+
+@Component({
+    selector: 'app-mini-image-viewer',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatTooltipModule
+    ],
+    templateUrl: './mini-image-viewer.component.html',
+    styleUrls: ['./mini-image-viewer.component.scss']
+})
+export class MiniImageViewerComponent implements OnInit {
+    @Input() public imageSrc = 'https://www.navplan.ch/charts/LSZB_AD_INFO_1.png'; // TODO
+
+    protected scale = 1;
+
+
+    constructor() {
+    }
+
+
+    ngOnInit() {
+    }
+
+
+    protected zoomIn() {
+        this.scale *= 1.2;
+    }
+
+
+    protected zoomOut() {
+        this.scale /= 1.2;
+    }
+}
