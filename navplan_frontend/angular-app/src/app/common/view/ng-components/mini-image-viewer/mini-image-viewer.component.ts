e9e79660db4fe33344f144818e5b117ae2d2e466
opacopac <armand@tschanz.com>
2025-04-26 15:42:35 +0200

    adding upload chart functionality

--- navplan_frontend/angular-app/src/app/common/view/ng-components/mini-image-viewer/mini-image-viewer.component.ts
+++ navplan_frontend/angular-app/src/app/common/view/ng-components/mini-image-viewer/mini-image-viewer.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
 import {CommonModule} from '@angular/common';
@@ -17,6 +17,8 @@ import {CommonModule} from '@angular/common';
 })
 export class MiniImageViewerComponent implements OnInit {
     @Input() public imageSrc;
+    @Input() public isClickable = false;
+    @Output() public imageClicked = new EventEmitter<{ x: number, y: number }>();
 
     protected scale = 1;
 
@@ -29,6 +31,14 @@ export class MiniImageViewerComponent implements OnInit {
     }
 
 
+    protected getImageStyle(): {[p: string]: any} | null | undefined {
+        return {
+            'transform' : 'scale(' + this.scale + ')',
+            'cursor' : this.isClickable ? 'crosshair' : 'auto',
+        };
+    }
+
+
     protected zoomIn() {
         this.scale *= 1.2;
     }
@@ -36,5 +46,21 @@ export class MiniImageViewerComponent implements OnInit {
 
     protected zoomOut() {
         this.scale /= 1.2;
+    }
+
+
+    protected onImageClicked(event: MouseEvent) {
+        if (!this.isClickable) {
+            return;
+        }
+
+        const imgElement = event.target as HTMLImageElement;
+        const rect = imgElement.getBoundingClientRect();
+        const clickX = event.clientX - rect.left;
+        const clickY = event.clientY - rect.top;
+        const adjustedX = clickX / this.scale;
+        const adjustedY = clickY / this.scale;
+
+        this.imageClicked.emit({x: adjustedX, y: adjustedY});
     }
 }
