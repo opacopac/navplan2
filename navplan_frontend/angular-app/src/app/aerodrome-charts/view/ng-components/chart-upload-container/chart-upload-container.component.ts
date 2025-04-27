8354bb35c3ca69fd04db9d2ee4ec36098c593cbc
opacopac <armand@tschanz.com>
2025-04-27 16:28:16 +0200

    adding upload chart functionality

--- navplan_frontend/angular-app/src/app/aerodrome-charts/view/ng-components/chart-upload-container/chart-upload-container.component.ts
+++ navplan_frontend/angular-app/src/app/aerodrome-charts/view/ng-components/chart-upload-container/chart-upload-container.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {MatButtonModule} from '@angular/material/button';
 import {MatStepperModule} from '@angular/material/stepper';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {FlightMapActions} from '../../../../flight-map/state/ngrx/flight-map.actions';
 import {
     getIsUploading,
@@ -21,24 +21,22 @@ import {ChartUploadParameters} from '../../../domain/model/chart-upload-paramete
     standalone: true,
     imports: [
         CommonModule,
+        ReactiveFormsModule,
         MatButtonModule,
         MatStepperModule,
-        ChartUploadStep1Component
+        ChartUploadStep1Component,
     ],
     templateUrl: './chart-upload-container.component.html',
     styleUrls: ['./chart-upload-container.component.scss']
 })
 export class ChartUploadContainerComponent implements OnInit {
+    public step1FormGroup: FormGroup;
+    public step2FormGroup: FormGroup;
+
     protected selectedFile$ = this.appStore.select(getSelectedChartFile);
     protected pdfParameters$ = this.appStore.select(getPdfParameters);
     protected uploadedChartInfo$ = this.appStore.select(getUploadedChartInfo);
     protected isUploading$ = this.appStore.select(getIsUploading);
-    protected firstFormGroup = this.formBuilder.group({
-        firstCtrl: ['', Validators.required],
-    });
-    protected secondFormGroup = this.formBuilder.group({
-        secondCtrl: ['', Validators.required],
-    });
 
 
     constructor(
@@ -49,6 +47,8 @@ export class ChartUploadContainerComponent implements OnInit {
 
 
     ngOnInit() {
+        this.step1FormGroup = this.formBuilder.group({});
+        this.step2FormGroup = this.formBuilder.group({});
     }
 
 
--- navplan_frontend/angular-app/src/app/aerodrome-charts/view/ng-components/chart-upload-step1/chart-upload-step1.component.ts
+++ navplan_frontend/angular-app/src/app/aerodrome-charts/view/ng-components/chart-upload-step1/chart-upload-step1.component.ts
@@ -11,7 +11,7 @@ import {
     MiniImageViewerComponent
 } from '../../../../common/view/ng-components/mini-image-viewer/mini-image-viewer.component';
 import {ChartUploadParameters} from '../../../domain/model/chart-upload-parameters';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormControl, FormGroup, FormGroupDirective, ReactiveFormsModule, Validators} from '@angular/forms';
 import {PdfParameters} from '../../../domain/model/pdf-parameters';
 import {IconButtonComponent} from '../../../../common/view/ng-components/icon-button/icon-button.component';
 import {ButtonColor} from '../../../../common/view/model/button-color';
@@ -44,21 +44,22 @@ export class ChartUploadStep1Component implements OnInit, OnChanges {
     @Output() fileSelected = new EventEmitter<File>();
     @Output() fileUploaded = new EventEmitter<ChartUploadParameters>();
 
+    protected formGroup: FormGroup;
     protected readonly ButtonColor = ButtonColor;
-    protected uploadStep1Form: FormGroup;
 
 
-    constructor(private formBuilder: FormBuilder) {
+    constructor(private parentForm: FormGroupDirective) {
     }
 
 
     ngOnInit() {
+        this.formGroup = this.parentForm.control;
         this.initForm();
     }
 
 
     ngOnChanges() {
-        this.initForm();
+        this.updateForm();
     }
 
 
@@ -68,7 +69,7 @@ export class ChartUploadStep1Component implements OnInit, OnChanges {
 
 
     protected onFileUploaded(file: File) {
-        const pdfParameters = this.getPdfParameters();
+        const pdfParameters = this.getPdfParametersFromForm();
         const chartUploadParameters = new ChartUploadParameters(file, pdfParameters);
 
         this.fileUploaded.emit(chartUploadParameters);
@@ -76,7 +77,7 @@ export class ChartUploadStep1Component implements OnInit, OnChanges {
 
 
     protected onPdfParamtersChanged() {
-        const pdfParameters = this.getPdfParameters();
+        const pdfParameters = this.getPdfParametersFromForm();
         const chartUploadParameters = new ChartUploadParameters(this.selectedChartFile, pdfParameters);
 
         this.fileUploaded.emit(chartUploadParameters);
@@ -84,56 +85,79 @@ export class ChartUploadStep1Component implements OnInit, OnChanges {
 
 
     private initForm() {
-        this.uploadStep1Form = this.formBuilder.group({
-            'chartName': [
-                this.uploadedChartInfo
-                    ? this.uploadedChartInfo.nameproposal
-                    : '',
-                [
-                    Validators.required,
-                    Validators.minLength(1),
-                    Validators.maxLength(100),
-                ]
-            ],
-            'pdfPage': [
-                this.pdfParameters
-                    ? this.pdfParameters.page + 1
-                    : PdfParameters.DEFAULT_PAGE + 1,
-                [
-                    Validators.required,
-                    Validators.min(1),
-                    Validators.max(99),
-                ]
-            ],
-            'pdfRotationDeg': [
-                this.pdfParameters && this.pdfParameters.rotation
-                    ? this.pdfParameters.rotation.deg
-                    : PdfParameters.DEFAULT_ROTATION_DEG,
-                [
-                    Validators.required,
-                    Validators.min(0),
-                    Validators.max(360),
-                ]
-            ],
-            'pdfDpi': [
-                this.pdfParameters
-                    ? this.pdfParameters.dpi
-                    : PdfParameters.DEFAULT_DPI,
-                [
-                    Validators.required,
-                    Validators.min(1),
-                    Validators.max(600),
-                ]
-            ],
-        });
+        if (!this.formGroup) {
+            return;
+        }
+
+        this.formGroup.addControl('chartName', new FormControl(
+            '',
+            [
+                Validators.required,
+                Validators.minLength(1),
+                Validators.maxLength(100),
+            ]
+        ));
+        this.formGroup.addControl('pdfPage', new FormControl(
+            PdfParameters.DEFAULT_PAGE + 1,
+            [
+                Validators.required,
+                Validators.min(1),
+                Validators.max(99),
+            ]
+        ));
+        this.formGroup.addControl('pdfRotationDeg', new FormControl(
+            PdfParameters.DEFAULT_ROTATION_DEG,
+            [
+                Validators.required,
+                Validators.min(0),
+                Validators.max(360),
+            ]
+        ));
+
+        this.formGroup.addControl('pdfDpi', new FormControl(
+            PdfParameters.DEFAULT_DPI,
+            [
+                Validators.required,
+                Validators.min(1),
+                Validators.max(600),
+            ]
+        ));
     }
 
 
-    private getPdfParameters(): PdfParameters {
+    private updateForm() {
+        if (!this.formGroup) {
+            return;
+        }
+
+        this.formGroup.get('chartName')?.setValue(
+            this.uploadedChartInfo
+                ? this.uploadedChartInfo.nameproposal
+                : ''
+        );
+        this.formGroup.get('pdfPage')?.setValue(
+            this.pdfParameters
+                ? this.pdfParameters.page + 1
+                : PdfParameters.DEFAULT_PAGE + 1
+        );
+        this.formGroup.get('pdfRotationDeg')?.setValue(
+            this.pdfParameters && this.pdfParameters.rotation
+                ? this.pdfParameters.rotation.deg
+                : PdfParameters.DEFAULT_ROTATION_DEG
+        );
+        this.formGroup.get('pdfDpi')?.setValue(
+            this.pdfParameters
+                ? this.pdfParameters.dpi
+                : PdfParameters.DEFAULT_DPI
+        );
+    }
+
+
+    protected getPdfParametersFromForm(): PdfParameters {
         return new PdfParameters(
-            this.uploadStep1Form.get('pdfPage').value - 1,
-            Angle.fromDeg(this.uploadStep1Form.get('pdfRotationDeg').value),
-            this.uploadStep1Form.get('pdfDpi').value
+            this.formGroup.get('pdfPage').value - 1,
+            Angle.fromDeg(this.formGroup.get('pdfRotationDeg').value),
+            this.formGroup.get('pdfDpi').value
         );
     }
 }
