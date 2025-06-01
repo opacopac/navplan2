61c8da7c10b076b34ce54ebc5cb2e7057b811cf0
opacopac <armand@tschanz.com>
2025-06-01 19:24:44 +0200

    adding upload chart functionality

--- navplan_backend/php-app/src/Navplan/AerodromeChart/Domain/Service/SwissGridChartTransformerService.php
+++ navplan_backend/php-app/src/Navplan/AerodromeChart/Domain/Service/SwissGridChartTransformerService.php
@@ -5,6 +5,7 @@ namespace Navplan\AerodromeChart\Domain\Service;
 use InvalidArgumentException;
 use Navplan\AerodromeChart\Domain\Model\ChartProjectionResult;
 use Navplan\AerodromeChart\Domain\Model\ChartRegistration;
+use Navplan\AerodromeChart\Domain\Model\ChartRegistrationType;
 use Navplan\AerodromeChart\Domain\Model\WorldFileInfo;
 use Navplan\Common\Domain\Model\Position2d;
 use Navplan\Common\Domain\SwissTopo\Lv03Coordinate;
@@ -26,18 +27,21 @@ class SwissGridChartTransformerService implements ISwissGridChartTransformerServ
 
     public function createChartProjektion(string $chartFile, ChartRegistration $chartReg): ChartProjectionResult
     {
-        $coord1 = Lv03Coordinate::fromLatLon($chartReg->geoCoord1->toLatLon());
-        $coord2 = Lv03Coordinate::fromLatLon($chartReg->geoCoord2->toLatLon());
+        switch ($chartReg->registrationType) {
+            case ChartRegistrationType::POS1_POS2:
+                $options = $this->getOptionsPos1Pos2($chartReg);
+                break;
+            case ChartRegistrationType::POS1_SCALE:
+            case ChartRegistrationType::ARP_SCALE:
+                $options = $this->getOptionsPos1Scale($chartReg);
+                break;
+            default:
+                $error = "Unsupported chart registration type: " . $chartReg->registrationType->value;
+                $this->loggingService->error($error);
+                throw new InvalidArgumentException($error);
+        };
+
         $exe = "/var/www/html/tools/swissgrid_chart_transformer";
-        $options = "-r "
-            . $chartReg->pixelXy1->getIntX() . " "
-            . $chartReg->pixelXy1->getIntY() . " "
-            . $coord1->getE() . " "
-            . $coord1->getN() . " "
-            . $chartReg->pixelXy2->getIntX() . " "
-            . $chartReg->pixelXy2->getIntY() . " "
-            . $coord2->getE() . " "
-            . $coord2->getN();
         $outputChart = $this->fileService->appendFilename($chartFile, "_output");
         $command = "$exe $options --chart $chartFile --output $outputChart";
 
@@ -51,6 +55,37 @@ class SwissGridChartTransformerService implements ISwissGridChartTransformerServ
             $outputChart,
             $worldFileInfo
         );
+    }
+
+
+    private function getOptionsPos1Pos2(ChartRegistration $chartReg): string
+    {
+        $coord1 = Lv03Coordinate::fromLatLon($chartReg->geoCoord1->toLatLon());
+        $coord2 = Lv03Coordinate::fromLatLon($chartReg->geoCoord2->toLatLon());
+
+        return "-r "
+            . $chartReg->pixelXy1->getIntX() . " "
+            . $chartReg->pixelXy1->getIntY() . " "
+            . $coord1->getE() . " "
+            . $coord1->getN() . " "
+            . $chartReg->pixelXy2->getIntX() . " "
+            . $chartReg->pixelXy2->getIntY() . " "
+            . $coord2->getE() . " "
+            . $coord2->getN();
+    }
+
+
+    private function getOptionsPos1Scale(ChartRegistration $chartReg): string
+    {
+        $coord1 = Lv03Coordinate::fromLatLon($chartReg->geoCoord1->toLatLon());
+
+        return "-t "
+            . $chartReg->pixelXy1->getIntX() . " "
+            . $chartReg->pixelXy1->getIntY() . " "
+            . $coord1->getE() . " "
+            . $coord1->getN() . " "
+            . $chartReg->scale . " "
+            . "200"; // TODO: DPI
     }
 
 
