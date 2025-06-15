85789aead53d540ce13b000abee70992c978b6d9
opacopac <armand@tschanz.com>
2025-06-15 16:44:25 +0200

    adding swisstopo service

--- navplan_frontend/angular-app/src/app/geo-physics/domain/service/swisstopo/swiss-topo-service.spec.ts
+++ navplan_frontend/angular-app/src/app/geo-physics/domain/service/swisstopo/swiss-topo-service.spec.ts
@@ -11,16 +11,16 @@ describe('SwissTopoService', () => {
 
     it('calculates the LV03 coordinates based on a WGS84 coordinate', () => {
         // given
-        const pos = new Position2d(7.0, 47.0);
+        const wgs84Pos = new Position2d(7.0, 47.0);
 
         // when
-        const lv03Pos = SwissTopoService.toCh03(pos);
+        const result = SwissTopoService.wsg84ToLv03(wgs84Pos);
 
         // then
-        expect(lv03Pos).toBeDefined();
-        expect(lv03Pos.getType()).toBe(GeoCoordinateType.LV03);
-        expect(lv03Pos.getE()).toBeCloseTo(566639.448, 0);
-        expect(lv03Pos.getN()).toBeCloseTo(205531.529, 0);
+        expect(result).toBeDefined();
+        expect(result.getType()).toBe(GeoCoordinateType.LV03);
+        expect(result.getE()).toBeCloseTo(566639.448, 0);
+        expect(result.getN()).toBeCloseTo(205531.529, 0);
     });
 
 
@@ -29,12 +29,72 @@ describe('SwissTopoService', () => {
         const pos = new GenericGeoCoordinate(GeoCoordinateType.LV03, 600000, 200000);
 
         // when
-        const wgs84Pos = SwissTopoService.toWgs84(pos);
+        const result = SwissTopoService.lv03ToWgs84(pos);
 
         // then
-        expect(wgs84Pos).toBeDefined();
-        expect(wgs84Pos.getType()).toBe(GeoCoordinateType.LON_LAT);
-        expect(wgs84Pos.longitude).toBeCloseTo(7.438632421, 5);
-        expect(wgs84Pos.latitude).toBeCloseTo(46.951082773, 5);
+        expect(result).toBeDefined();
+        expect(result.getType()).toBe(GeoCoordinateType.LON_LAT);
+        expect(result.longitude).toBeCloseTo(7.438632421, 5);
+        expect(result.latitude).toBeCloseTo(46.951082773, 5);
+    });
+
+
+    it('calculates the WGS84 coordinates based on a LV95 coordinate', () => {
+        // given
+        const lv95pos = new GenericGeoCoordinate(GeoCoordinateType.LV95, 2600000, 1200000);
+
+        // when
+        const result = SwissTopoService.lv95ToWgs84(lv95pos);
+
+        // then
+        expect(result).toBeDefined();
+        expect(result.getType()).toBe(GeoCoordinateType.LON_LAT);
+        expect(result.longitude).toBeCloseTo(7.438632421, 5);
+        expect(result.latitude).toBeCloseTo(46.951082773, 5);
+    });
+
+
+    it('calculates the LV95 coordinates based on a WGS84 coordinate', () => {
+        // given
+        const wgs84Pos = new Position2d(7.0, 47.0);
+
+        // when
+        const result = SwissTopoService.Wgs84ToLv95(wgs84Pos);
+
+        // then
+        expect(result).toBeDefined();
+        expect(result.getType()).toBe(GeoCoordinateType.LV95);
+        expect(result.getE()).toBeCloseTo(2566639.343, 0);
+        expect(result.getN()).toBeCloseTo(1205531.915, 0);
+    });
+
+
+    it('converts LV03 to LV95 coordinates', () => {
+        // given
+        const lv03Pos = new GenericGeoCoordinate(GeoCoordinateType.LV03, 600000, 200000);
+
+        // when
+        const result = SwissTopoService.lv03ToLv95(lv03Pos);
+
+        // then
+        expect(result).toBeDefined();
+        expect(result.getType()).toBe(GeoCoordinateType.LV95);
+        expect(result.getE()).toBe(2600000);
+        expect(result.getN()).toBe(1200000);
+    });
+
+
+    it('converts LV95 to LV03 coordinates', () => {
+        // given
+        const lv95Pos = new GenericGeoCoordinate(GeoCoordinateType.LV95, 2600000, 1200000);
+
+        // when
+        const result = SwissTopoService.lv95ToLv03(lv95Pos);
+
+        // then
+        expect(result).toBeDefined();
+        expect(result.getType()).toBe(GeoCoordinateType.LV03);
+        expect(result.getE()).toBe(600000);
+        expect(result.getN()).toBe(200000);
     });
 });
--- navplan_frontend/angular-app/src/app/geo-physics/domain/service/swisstopo/swiss-topo-service.ts
+++ navplan_frontend/angular-app/src/app/geo-physics/domain/service/swisstopo/swiss-topo-service.ts
@@ -5,7 +5,11 @@ import {GeoCoordinateType} from '../../../../aerodrome-charts/domain/model/geo-c
 import {Swisstopo} from './wgs84_ch1903';
 
 export class SwissTopoService {
-    public static toCh03(pos: Position2d): GeoCoordinate {
+    private static LV95_OFFSET_E = 2000000;
+    private static LV95_OFFSET_N = 1000000;
+
+
+    public static wsg84ToLv03(pos: Position2d): GeoCoordinate {
         if (!pos) {
             return null;
         }
@@ -16,7 +20,7 @@ export class SwissTopoService {
     }
 
 
-    public static toWgs84(pos: GeoCoordinate): Position2d {
+    public static lv03ToWgs84(pos: GeoCoordinate): Position2d {
         if (!pos) {
             return null;
         }
@@ -24,5 +28,53 @@ export class SwissTopoService {
         const [lon, lat] = Swisstopo.CHtoWGS(pos.getE(), pos.getN());
 
         return new Position2d(lon, lat);
+    }
+
+
+    public static lv95ToWgs84(pos: GeoCoordinate): Position2d {
+        if (!pos) {
+            return null;
+        }
+
+        const lv03Pos = SwissTopoService.lv95ToLv03(pos);
+
+        return SwissTopoService.lv03ToWgs84(lv03Pos);
+    }
+
+
+    public static Wgs84ToLv95(pos: Position2d): GeoCoordinate {
+        if (!pos) {
+            return null;
+        }
+
+        const lv03Pos = SwissTopoService.wsg84ToLv03(pos);
+
+        return SwissTopoService.lv03ToLv95(lv03Pos);
+    }
+
+
+    public static lv03ToLv95(pos: GeoCoordinate): GeoCoordinate {
+        if (!pos) {
+            return null;
+        }
+
+        return new GenericGeoCoordinate(
+            GeoCoordinateType.LV95,
+            pos.getE() + SwissTopoService.LV95_OFFSET_E,
+            pos.getN() + SwissTopoService.LV95_OFFSET_N
+        );
+    }
+
+
+    public static lv95ToLv03(pos: GeoCoordinate): GeoCoordinate {
+        if (!pos) {
+            return null;
+        }
+
+        return new GenericGeoCoordinate(
+            GeoCoordinateType.LV03,
+            pos.getE() - SwissTopoService.LV95_OFFSET_E,
+            pos.getN() - SwissTopoService.LV95_OFFSET_N
+        );
     }
 }
