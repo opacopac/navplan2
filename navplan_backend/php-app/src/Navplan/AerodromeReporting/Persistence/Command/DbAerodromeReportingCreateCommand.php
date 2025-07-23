660183d5f5f504be9ef5ceedf6ffc1d2238d91db
ue55617 <armand.tschanz@sbb.ch>
2025-07-23 16:56:04 +0200

    query builder

--- navplan_backend/php-app/src/Navplan/AerodromeReporting/Persistence/Command/DbAerodromeReportingCreateCommand.php
+++ navplan_backend/php-app/src/Navplan/AerodromeReporting/Persistence/Command/DbAerodromeReportingCreateCommand.php
@@ -6,7 +6,6 @@ use Navplan\AerodromeReporting\Domain\Command\IAerodromeReportingCreateCommand;
 use Navplan\AerodromeReporting\Domain\Model\ReportingPoint;
 use Navplan\AerodromeReporting\Persistence\Model\DbTableReportingPoints;
 use Navplan\System\Db\Domain\Service\IDbService;
-use Navplan\System\Db\MySql\DbHelper;
 
 
 class DbAerodromeReportingCreateCommand implements IAerodromeReportingCreateCommand
@@ -20,47 +19,25 @@ class DbAerodromeReportingCreateCommand implements IAerodromeReportingCreateComm
 
     public function create(ReportingPoint $reportingPoint, int $userId): ReportingPoint
     {
-        $query = $this->getInsertSql($reportingPoint, $userId);
+        $t = new DbTableReportingPoints();
+        $query = $this->dbService->getInsertCommandBuilder()
+            ->insertInto($t)
+            ->setValue($t->colType(), $reportingPoint->type)
+            ->setValue($t->colAdIcao(), $reportingPoint->airport_icao)
+            ->setValue($t->colName(), $reportingPoint->name)
+            ->setValue($t->colHeli(), $reportingPoint->heli)
+            ->setValue($t->colInbdComp(), $reportingPoint->inbd_comp)
+            ->setValue($t->colOutbdComp(), $reportingPoint->outbd_comp)
+            ->setValue($t->colMinFt(), $reportingPoint->alt_min?->ft ?? NULL)
+            ->setValue($t->colMaxFt(), $reportingPoint->alt_max?->ft ?? NULL)
+            ->setValue($t->colLat(), $reportingPoint->position?->latitude ?? NULL)
+            ->setValue($t->colLon(), $reportingPoint->position?->longitude ?? NULL)
+            ->setValue($t->colPolygon(), $reportingPoint->polygon?->toString() ?? "")
+            ->build();
+
         $this->dbService->execCUDQuery($query, "error creating reporting point");
         $reportingPoint->id = $this->dbService->getInsertId();
 
         return $reportingPoint;
-    }
-
-
-    private function getInsertSql(ReportingPoint $reportingPoint, int $userId): string
-    {
-        $query = "INSERT INTO " . DbTableReportingPoints::TABLE_NAME . " (";
-        $query .= join(",", [
-            DbTableReportingPoints::COL_TYPE,
-            DbTableReportingPoints::COL_AD_ICAO,
-            DbTableReportingPoints::COL_NAME,
-            DbTableReportingPoints::COL_HELI,
-            DbTableReportingPoints::COL_INBD_COMP,
-            DbTableReportingPoints::COL_OUTBD_COMP,
-            DbTableReportingPoints::COL_MIN_FT,
-            DbTableReportingPoints::COL_MAX_FT,
-            DbTableReportingPoints::COL_LAT,
-            DbTableReportingPoints::COL_LON,
-            DbTableReportingPoints::COL_POLYGON,
-            DbTableReportingPoints::COL_EXTENT
-        ]);
-        $query .= ") VALUES (";
-        $query .= join(", ", [
-            DbHelper::getDbStringValue($this->dbService, $reportingPoint->type),
-            DbHelper::getDbStringValue($this->dbService, $reportingPoint->airport_icao),
-            DbHelper::getDbStringValue($this->dbService, $reportingPoint->name),
-            DbHelper::getDbBoolValue($reportingPoint->heli),
-            DbHelper::getDbBoolValue($reportingPoint->inbd_comp),
-            DbHelper::getDbBoolValue($reportingPoint->outbd_comp),
-            DbHelper::getDbIntValue($reportingPoint->alt_min?->ft ?? NULL),
-            DbHelper::getDbIntValue($reportingPoint->alt_max?->ft ?? NULL),
-            DbHelper::getDbFloatValue($reportingPoint->position?->latitude ?? NULL),
-            DbHelper::getDbFloatValue($reportingPoint->position?->longitude ?? NULL),
-            DbHelper::getDbStringValue($this->dbService, $reportingPoint->polygon?->toString() ?? ""),
-        ]);
-        $query .= ")";
-
-        return $query;
     }
 }
