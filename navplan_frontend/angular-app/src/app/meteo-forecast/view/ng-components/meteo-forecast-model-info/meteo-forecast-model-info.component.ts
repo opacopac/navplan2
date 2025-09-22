e0ba90a98ed750a2b319d1f167608048d55165ee
ue55617 <armand.tschanz@sbb.ch>
2025-09-22 17:58:59 +0200

    forecast model info

--- navplan_frontend/angular-app/src/app/meteo-forecast/view/ng-components/meteo-forecast-model-info/meteo-forecast-model-info.component.ts
+++ navplan_frontend/angular-app/src/app/meteo-forecast/view/ng-components/meteo-forecast-model-info/meteo-forecast-model-info.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ForecastRun} from '../../../domain/model/forecast-run';
+import {WeatherModelType} from '../../../domain/model/weather-model-type';
+import {StringnumberHelper} from '../../../../system/domain/service/stringnumber/stringnumber-helper';
 
 
 @Component({
@@ -17,5 +19,39 @@ export class MeteoForecastModelInfoComponent implements OnInit {
 
 
     ngOnInit(): void {
+    }
+
+
+    protected getModelName(): string {
+        if (!this.currentForecastRun) {
+            return '';
+        }
+
+        switch (this.currentForecastRun.model.modelType) {
+            case WeatherModelType.ICON_D2:
+                return 'DWD ICON-D2';
+            case WeatherModelType.ICON_EU:
+                return 'DWD ICON-EU';
+            case WeatherModelType.ICON:
+                return 'DWD ICON Global';
+            case WeatherModelType.ICON_CH1:
+                return 'MeteoSwiss ICON-CH1-EPS';
+            default:
+                return '';
+        }
+    }
+
+
+    protected getRunName(): string {
+        if (!this.currentForecastRun) {
+            return '';
+        }
+
+        const startTime = this.currentForecastRun.startTime;
+
+        return startTime.getUTCFullYear() + '-'
+            + StringnumberHelper.zeroPad(startTime.getUTCMonth() + 1, 2) + '-'
+            + StringnumberHelper.zeroPad(startTime.getUTCDate(), 2) + ' '
+            + StringnumberHelper.zeroPad(startTime.getUTCHours(), 2) + ':00 UTC';
     }
 }
