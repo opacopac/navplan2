0bfaf6d675fca7d728431de3dcd1d75a78347ccc
armand <armand@tschanz.com>
2025-12-08 21:38:10 +0100

    add meteo_forecast volume and copy script for data transfer

---
+++ navplan_backend/copy_meteo_forecast.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set SOURCE_PATH=d:\work\own\projects\grib2_meteo_charts\output
+set CONTAINER_NAME=navplan_backend
+set CONTAINER_DEST=/var/www/html/meteo_forecast/
+
+echo Copying meteo_forecast data to docker volume.
+echo Source: %SOURCE_PATH%
+echo Destination: %CONTAINER_NAME%:%CONTAINER_DEST%
+echo This may take several minutes...
+echo.
+
+docker cp "%SOURCE_PATH%\." %CONTAINER_NAME%:%CONTAINER_DEST%
+
+if %ERRORLEVEL% EQU 0 (
+    echo.
+    echo Successfully copied meteo_forecast data to volume!
+) else (
+    echo.
+    echo Error: Failed to copy files. Make sure:
+    echo 1. The %CONTAINER_NAME% container is running
+    echo 2. The source folder %SOURCE_PATH% exists
+    echo.
+    echo You can start the containers with: docker-compose up -d
+    exit /b 1
+)
+
+echo.
+pause
