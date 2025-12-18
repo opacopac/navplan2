a5dec114d4ee06a9e0aa8feec6b2cd9f807cbb03
armand <armand@tschanz.com>
2025-12-18 22:56:27 +0100

    feat: simplify NOTAM display by removing title and validity methods, using dedicated component for content

--- navplan_frontend/angular-app/src/app/notam/view/ng-components/map-popup-notam/map-popup-notam.component.ts
+++ navplan_frontend/angular-app/src/app/notam/view/ng-components/map-popup-notam/map-popup-notam.component.ts
@@ -3,13 +3,14 @@ import {Notam} from '../../../domain/model/notam';
 import {Position2d} from '../../../../geo-physics/domain/model/geometry/position2d';
 import {OlOverlayBaseComponent} from '../../../../base-map/view/ng-components/ol-overlay-base.component';
 import {MatCardModule} from '@angular/material/card';
-import {StringnumberHelper} from '../../../../system/domain/service/stringnumber/stringnumber-helper';
+import {MapPopupNotamListComponent} from '../map-popup-notam-list/map-popup-notam-list.component';
 
 
 @Component({
     selector: 'app-map-popup-notam',
     imports: [
-        MatCardModule
+        MatCardModule,
+        MapPopupNotamListComponent
     ],
     templateUrl: './map-popup-notam.component.html',
     styleUrls: ['./map-popup-notam.component.scss']
@@ -32,72 +33,5 @@ export class MapPopupNotamComponent extends OlOverlayBaseComponent implements On
         this.notam = notam;
         this.setPosition(clickPos);
         this.markForCheck();
-    }
-
-
-    public getTitle(): string {
-        if (!this.notam || !this.notam.isIcaoFormat) {
-            return 'NOTAM: ' + this.notam?.idnotam;
-        } else if (this.notam.qCode.indexOf('XX') === 0) {
-            return 'NOTAM: ' + this.notam.idnotam;
-        } else if (this.notam.qCode.indexOf('XX') === 2) {
-            return this.notam.subject;
-        } else {
-            return this.notam.subject + ' ' + this.notam.modifier;
-        }
-    }
-
-
-    public getNotamText(): string {
-        return this.notam.fullNotam.replace(/\n/g, '<br />');
-    }
-
-
-    public getNotamValidity(): string {
-        let fromText, tillText: string;
-
-        // TODO: move date parsing to import
-        if (this.notam.isIcaoFormat) {
-            const timeRegFrom = /\sB\)\s+((\d\d)(\d\d)(\d\d)(\d\d)(\d\d))\s/im;
-            const result1 = this.notam.fullNotam.match(timeRegFrom);
-
-            const timeRegTill = /\sC\)\s+((\d\d)(\d\d)(\d\d)(\d\d)(\d\d)|PERM)\s/im;
-            const result2 = this.notam.fullNotam.match(timeRegTill);
-
-            if (result1 && result2) {
-                const d1 = this.getUtcDate(result1);
-                fromText = this.getLtString(d1);
-
-                if (result2[1] !== 'PERM') {
-                    const d2 = this.getUtcDate(result2);
-                    tillText = this.getLtString(d2);
-                } else {
-                    tillText = result2[1];
-                }
-            }
-        } else {
-            fromText = new Date(this.notam.startDate).toLocaleDateString();
-            tillText = new Date(this.notam.endDate).toLocaleDateString();
-        }
-
-        return '(' + fromText + ' - ' + tillText + ')';
-    }
-
-
-    private getUtcDate(result): Date {
-        return new Date(Date.UTC(
-            parseInt('20' + result[2], 10),
-            parseInt(result[3], 10) - 1,
-            parseInt(result[4], 10),
-            parseInt(result[5], 10),
-            parseInt(result[6], 10)
-        ));
-    }
-
-
-    private getLtString(date: Date): string {
-        const datePart = date.toLocaleDateString();
-        const timePart = StringnumberHelper.zeroPad(date.getHours()) + ':' + StringnumberHelper.zeroPad(date.getMinutes());
-        return datePart + ' ' + timePart + ' LT';
     }
 }
