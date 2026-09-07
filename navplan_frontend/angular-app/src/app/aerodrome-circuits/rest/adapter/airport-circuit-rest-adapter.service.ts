be20e3f11119cc5713ee38b388b36128b78f4069
armand <armand@tschanz.com>
2026-09-07 22:30:31 +0200

    refactor AirportCircuitRestAdapterService to extend RestReadByExtentService for improved code reuse and maintainability

--- navplan_frontend/angular-app/src/app/aerodrome-circuits/rest/adapter/airport-circuit-rest-adapter.service.ts
+++ navplan_frontend/angular-app/src/app/aerodrome-circuits/rest/adapter/airport-circuit-rest-adapter.service.ts
@@ -1,40 +1,28 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {LoggingService} from '../../../system/domain/service/logging/logging.service';
 import {Extent2d} from '../../../geo-physics/domain/model/geometry/extent2d';
 import {AirportCircuit} from '../../domain/model/airport-circuit';
 import {RestAirportCircuitConverter} from '../converter/rest-airport-circuit-converter';
 import {IRestAirportCircuit} from '../model/i-rest-airport-circuit';
 import {IAirportCircuitRepoService} from '../../domain/service/i-airport-circuit-repo.service';
-import {RestExtent2dConverter} from '../../../geo-physics/rest/model/rest-extent2d-converter';
-import {RestZoomConverter} from '../../../geo-physics/rest/model/rest-zoom-converter';
-import {HttpHelper} from '../../../system/domain/service/http/http-helper';
+import {RestReadByExtentService} from '../../../common/rest/service/rest-read-by-extent.service';
 
 
 @Injectable()
-export class AirportCircuitRestAdapterService implements IAirportCircuitRepoService {
-    constructor(private http: HttpClient) {
+export class AirportCircuitRestAdapterService extends RestReadByExtentService<AirportCircuit, IRestAirportCircuit> implements IAirportCircuitRepoService {
+    constructor(http: HttpClient) {
+        super(http, environment.airportCircuitApiBaseUrl, 'ERROR reading airport circuits by extent');
     }
 
 
     public readAirportCircuitsByExtent(extent: Extent2d, zoom: number): Observable<AirportCircuit[]> {
-        const params = HttpHelper.mergeParameters([
-            RestExtent2dConverter.getUrlParams(extent),
-            RestZoomConverter.getUrlParam(zoom)
-        ]);
-        const url: string = environment.airportCircuitApiBaseUrl;
+        return this.readByExtent(extent, zoom);
+    }
 
-        return this.http
-            .get<IRestAirportCircuit[]>(url, { params })
-            .pipe(
-                map((response) => RestAirportCircuitConverter.fromRestList(response)),
-                catchError(err => {
-                    LoggingService.logResponseError('ERROR reading airport circuits by extent', err);
-                    return throwError(err);
-                })
-            );
+
+    protected convertList(restItems: IRestAirportCircuit[]): AirportCircuit[] {
+        return RestAirportCircuitConverter.fromRestList(restItems);
     }
 }
